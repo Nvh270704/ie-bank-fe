3089febafafda6b0567af8dd073b233b9af76ea8
Noah Valderrama <noahvaderrama@gmail.com>
2024-10-14 18:53:46 +0200

    home.vue added

---
+++ src/components/home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1>Welcome to the Bank Account Management System</h1>
+      <button @click="goToAccounts">Go to Accounts</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home',
+    methods: {
+      goToAccounts() {
+        this.$router.push('/accounts');
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your styles here if needed */
+  </style>
+  
